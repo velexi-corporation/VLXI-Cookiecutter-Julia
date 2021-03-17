32c5b8d58324d0eb02b1fe2a07c79735a3909479
Kevin Chu <kevin@velexi.com>
2021-03-16 21:47:59 -0700

    Add template parameters to license section of sample files.

--- src/ExampleType.jl
+++ src/ExampleType.jl
@@ -2,11 +2,11 @@
 ExampleType.jl defines the ExampleType type and core methods
 
 ------------------------------------------------------------------------------
-COPYRIGHT/LICENSE. This file is part of the XYZ package. It is subject to
-the license terms in the LICENSE file found in the top-level directory of
-this distribution. No part of the XYZ package, including this file, may be
-copied, modified, propagated, or distributed except according to the terms
-contained in the LICENSE file.
+COPYRIGHT/LICENSE. This file is part of the {{ PKG_NAME }} package. It is
+subject to the license terms in the LICENSE file found in the root directory
+of this distribution. No part of the {{ PKG_NAME }} package, including this
+file, may be copied, modified, propagated, or distributed except according to
+the terms contained in the LICENSE file.
 ------------------------------------------------------------------------------
 """
 # --- Exports
