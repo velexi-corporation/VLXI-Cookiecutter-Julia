d8cee3a455254ca3e0c8f3ebc5a9942954904b8b
Kevin Chu <kevin@velexi.com>
2021-09-03 16:41:07 -0700

    Polish source code license incantations.

--- src/ExampleMethods.jl
+++ src/ExampleMethods.jl
@@ -1,13 +1,12 @@
 """
 ExampleMethods.jl defines methods for the ExampleModule.jl module.
 
-------------------------------------------------------------------------------
-COPYRIGHT/LICENSE. This file is part of the {{ PKG_NAME }} package. It is
-subject to the license terms in the LICENSE file found in the root directory
-of this distribution. No part of the {{ PKG_NAME }} package, including this
-file, may be copied, modified, propagated, or distributed except according to
-the terms contained in the LICENSE file.
-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------
+COPYRIGHT/LICENSE. This file is part of the {{ PKG_NAME }} package. It is subject to the
+license terms in the LICENSE file found in the root directory of this distribution. No
+part of the {{ PKG_NAME }} package, including this file, may be copied, modified,
+propagated, or distributed except according to the terms contained in the LICENSE file.
+-------------------------------------------------------------------------------------------
 """
 # --- Exports
 
